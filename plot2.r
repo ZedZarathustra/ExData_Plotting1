50148b4614edc6737ebef38e0524477b06287bfd
Scott Zwick <sdzwick@nppd.com>
2018-01-22 14:47:00 -0600

    Date col converted to date format

--- plot2.r
+++ plot2.r
@@ -10,7 +10,7 @@ setwd("~/R/Coursera/4-Exploratory_Data_Analysis/ExData_Plotting1")
      ep2 <- subset(elec_pow, Date == "2/2/2007")
      ep_2days <- rbind(ep1, ep2)
 ##Convert format of date & time columns
-     ep_2days$Date <- as.Date(ep_2days$Date , "%d/%m/%y")
+     ep_2days$Date <- as.Date(ep_2days$Date, format = "%d/%m/%Y")
      ## plot line graph
 ##png(filename = "plot1.png", width = 480, height = 480)
 with(ep_2days, plot(Global_active_power, time))
